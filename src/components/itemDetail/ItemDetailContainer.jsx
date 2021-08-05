695377d63e277ee1783ce1a4b28025507fd1dd41
Pedro Palencia <pedropalencia7@gmail.com>
2021-08-04 22:23:48 -0300

    se crea firebaseService.js

--- src/components/itemDetail/ItemDetailContainer.jsx
+++ src/components/itemDetail/ItemDetailContainer.jsx
@@ -3,12 +3,19 @@ import { useParams } from 'react-router'
 import { getData } from '../../data/getData'
 import ItemDetail from "./ItemDetail"
 import Spinner from 'react-bootstrap/spinner'
+import { getFirestore } from '../../data/firebaseService'
 import "./ItemDetail.css"
 
 function ItemDetailContainer() {
     
     const [item, setItem] = useState(false)
     const { itemId } = useParams()
+
+    useEffect(() => {
+        const dbQuery = getFirestore()
+        dbQuery.collection('items').get()
+        .then(res => console.log(res))
+    }, [])
 
     useEffect(()=>{
         setItem(false)
